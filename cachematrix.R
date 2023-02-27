6c9c6635ffd5dc7f276449950ac5175dc2cb12a9
dbornas <46637445+dbornas@users.noreply.github.com>
2023-02-27 16:14:12 +0100

    Add files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,45 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This exercise is about lexical programming. I have designed 2 functions
+## that calculate the inverse of a matrix and keep it in memory. When the computation
+## is requested afterwards, the inverse is retrieved from memory and a message is issued.
+## Otherwise, the inverse is computed.
 
-## Write a short comment describing this function
-
+## The makeCacheMatrix creates a new environment with a list of 4 functions and 2 matrixes
+## objects x and m which are passed to the global environemnt with the operator "<<-".
 makeCacheMatrix <- function(x = matrix()) {
-
+    m <- NULL
+    set <- function(y) {
+        x <<- y
+        m <<- NULL
+    }
+    get <- function() x
+    setinv <- function(inv) m <<- inv
+    getinv <- function() m
+    list(set = set, 
+        get = get,
+        setinv = setinv,
+        getinv = getinv)
 }
 
-
-## Write a short comment describing this function
-
+## The cacheSolve function calculates  the inverse of a squared matrix.
+## This gets input from the result of 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+## Return a matrix that is the inverse of 'x'
+    m <- x$getinv()
+    if(!is.null(m)) {
+        message("The inverse matrix is in cache. No need to compute again.")
+        return(m)
+    }
+    data <- x$get()
+    m <- solve(data, ...)
+    x$setinv(m)
+    m
 }
+
+x1<-matrix(1:4,nrow=2,ncol=2)
+myMatrix <- makeCacheMatrix(x1)
+inverse_matrix<-cacheSolve(myMatrix)
+x1
+inverse_matrix
+x1%*%inverse_matrix
+inverse_matrix<-cacheSolve(myMatrix)
+
